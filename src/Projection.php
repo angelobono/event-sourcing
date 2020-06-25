b84c47a08ac43d1f3793b31eb9ef5a63a4b9b786
Angelo Bono <mail@angelobono.de>
2020-06-25 23:27:05 +0200

    Update Projection.php

--- src/Projection.php
+++ src/Projection.php
@@ -3,22 +3,40 @@
  * @Author: Angelo Bono
  * @Date:   2020-03-13 19:22:19
  * @Last Modified by:   Angelo Bono
- * @Last Modified time: 2020-03-13 19:33:21
+ * @Last Modified time: 2020-06-25 23:13:57
  */
 
 namespace Bono\EventSourcing;
 
 class Projection
 {
+    private $eventStore;
+    private $topic = '';
+    private $data = [];
+
+    public function __construct(EventStore $eventStore)
+    {
+        $this->eventStore = $eventStore;
+    }
+
     public function from(string $topic)
     {
+        $this->topic = $topic;
+        return $this;
     }
 
     public function onInit(callable $init)
     {
+        $this->data = $init;
+        return $this;
     }
 
-    public function when(callable ...$handler)
+    public function when(callable ...$handlers)
     {
+        foreach ($this->eventStore->read($topic) as $event) {
+            foreach ($handlers as $handler) {
+                $handler($event, $this->data);
+            }
+        }
     }
 }
